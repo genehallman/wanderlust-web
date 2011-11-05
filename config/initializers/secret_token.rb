a9ba5dbf14c7475fd57dd8da1db64a64860608c0
Gene Hallman <gene@flickme.com>
2011-11-05 12:46:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = 'e5bb6c18aaa98c945d77b57656082f9241cb8f471747f67a9ca96505b4a9915f4155ba0b68914b336df5204abbad48b9c7020a22d40fc383faf0bbf5421e39c2'
