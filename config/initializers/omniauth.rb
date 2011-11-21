756a7d48fd15d681f147e07335214c0a56404a8c
How <how@how-UL80AG.(none)>
2011-11-20 16:03:47 -0800

    facebook keys added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '###', '####'
+  provider :facebook, '184428114976886', 'dfff3a73d2f130f5d5a45a21a42ab788'
 end
 
 
